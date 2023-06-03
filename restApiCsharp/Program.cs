269feab291dd10a2da87df744ec28a95f9d9210c
Who7F <34862598+Who7F@users.noreply.github.com>
2023-06-03 22:30:55 +0100

    Going slow

--- restApiCsharp/Program.cs
+++ restApiCsharp/Program.cs
@@ -1,3 +1,27 @@
+
+using Microsoft.AspNetCore.Identity;
+using System;
+using Microsoft.Data.SqlClient;
+//Add `Microsoft.Data.SqlClient` package
+
+try
+{
+    SqlConnectionStringBuilder myBuilder = new SqlConnectionStringBuilder();
+    myBuilder.DataSource = "localhost - Not telling";
+    myBuilder.UserID = "SQLEXPRESS - Not telling";
+    myBuilder.TrustServerCertificate = true;
+    
+    myBuilder.InitialCatalog = "TeaShop";
+    Console.WriteLine(myBuilder.ConnectionString.ToString());//check string
+    //using (SqlConnection connection = new SqlConnection(myBuilder.ConnectionString))
+}
+catch (SqlException e) 
+{
+    Console.WriteLine(e.ToString());
+}
+
+
+
 var app = WebApplication.Create();
 
 app.MapGet("/people", () => new[]
@@ -9,6 +33,12 @@ app.MapGet("/tea", () => new[]
 {
     new Tea("Builder"), new("Earl Grey, Hot")
 });
+
+app.MapDelete("/todo", () => { });
+
+app.MapPost("/todo", () => { });
+
+app.MapPut("/todo", () => { });
 
 
 
